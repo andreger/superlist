e6ccc47b31f7232f9c402a6dc55f118b8e52acd8
André Gervásio <andreger@gmail.com>
2025-09-14 09:12:38 -0300

    fix: Fixed init configs in app

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3200,
+    host: true, // This allows Vite to accept connections from any host (needed for Docker)
+  },
+});
